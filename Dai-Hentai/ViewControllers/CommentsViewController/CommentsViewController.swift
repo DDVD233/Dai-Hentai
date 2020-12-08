689f8bf13a9dc56266bce7901a9dc4e010c2bfe0
zjdavid <zjdavid.2003@gmail.com>
2020-12-08 10:17:08 +0800

    CommentCell & CommentVC

---
+++ Dai-Hentai/ViewControllers/CommentsViewController/CommentsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CommentsViewController.swift
+//  Dai-Hentai
+//
+//  Created by DDavid on 12/8/20.
+//  Copyright © 2020 DaidoujiChen. All rights reserved.
+//
+
+import Foundation
+
+class CommentsViewController: UITableViewController {
+    
+}
---
+++ Dai-Hentai/Views/CommentCell/CommentCell.swift
@@ -0,0 +1,20 @@
+//
+//  CommentCell.swift
+//  Dai-Hentai
+//
+//  Created by DDavid on 12/8/20.
+//  Copyright © 2020 DaidoujiChen. All rights reserved.
+//
+
+import Foundation
+
+class CommentCell: UICollectionViewCell {
+
+    // MARK: Property
+    
+    // MARK: Life Cycle
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+}
